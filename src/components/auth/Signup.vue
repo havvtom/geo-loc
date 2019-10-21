2475d1bcddce165c2964b5b4d5c6a195d2c226aa
Tom Havatyi <havvtom@gmail.com>
2019-10-21 08:52:25 +0200

    map and database

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -4,21 +4,22 @@
 			<h2 class="center deep-purple-text">Sign Up</h2>
 			<div class="row">
 		        <div class="input-field col s12">
-		          <input  type="email" id="email">
+		          <input  type="email" id="email" v-model="email">
 		          <label for="email">Email</label>
 	        	</div>
 	      	</div>
 	        <div class="row">
 	          <div class="input-field col s12">
-	            <input id="password" type="password">
+	            <input id="password" type="password" v-model="password">
 	            <label for="password">Password</label>
 	          </div>
 	        </div>
 	        <div class="row">
 	          <div class="input-field col s12">
-	            <input id="alias" type="text" class="validate">
+	            <input id="alias" type="text" v-model="alias">
 	            <label for="alias">Alias</label>
 	          </div>
+	          <p class="center red-text" v-if="feedback">{{feedback}}</p>
 	        </div>
 	        <div class="center">
 	        <button class="btn deep-purple" type="submit">Submit
@@ -30,16 +31,32 @@
 	</div>
 </template>
 <script type="text/javascript">
+	import slugify from "slugify";
+	import db from "@/firebase/init";
 	export default{
 		name: "Signup",
 			data(){
 				return {
+					email: "",
+					password: "",
+					alias: "",
+					feedback: "",
+					slug: ""
 
 			}
 		},
 		methods:{
 			signUp(){
-				console.log('submitted')
+				if(this.alias){
+					this.slug = slugify(this.alias, {
+						replacement: "-",
+						remove: /[$*_+~.()'"!\-:@]/g,
+						lower: true
+					})
+					console.log(this.slug);
+				}else{
+					this.feedback = "You need to enter an alias"
+				}
 			}
 		}
 	}
--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <a href="#" class="brand-logo">Geo-Loc</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="">Signup</a></li>
+          <li><router-link to="/signup">SignUp</router-link></li>
           <li><a href="">Login</a></li>
           
         </ul>
